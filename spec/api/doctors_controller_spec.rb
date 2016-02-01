37d26dab4f33dc5b3f705bebb31c2bcfd6190ef5
ShrutiNaik <shrutinaik1188@gmail.com>
2016-01-31 22:51:08 -0600

    Updating tests

--- spec/api/doctors_controller_spec.rb
+++ spec/api/doctors_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-describe "API authentication" , :type => :api do
+describe "DoctorsController" , type: :controller do
 
-  it "making a request without cookie token " do
+  it "makes a valid proxy call" do
     get "/api/v1/doctors/search/meenal",:formate =>:json
-    last_response.status.should eql(200)
-    #error = {:error=>'You need to sign in or sign up before continuing.'}
-    #last_response.body.should  eql(error.to_json)
+    response.status.should eql(200)
   end
 
 end  
