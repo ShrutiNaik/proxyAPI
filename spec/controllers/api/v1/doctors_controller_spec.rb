58a2e5cdfb7ac1ba4634311593d7ecb914b44ed3
ShrutiNaik <shrutinaik1188@gmail.com>
2016-02-01 00:16:58 -0600

    Update tests

--- spec/controllers/api/v1/doctors_controller_spec.rb
+++ spec/controllers/api/v1/doctors_controller_spec.rb
@@ -4,9 +4,22 @@ RSpec.describe Api::V1::DoctorsController, type: :controller do
 
   describe "GET #search" do
     it "returns http success" do
-      get :search, :doctor_name => 'meenal'
+      get :search, :doctor_name => 'ted'
       expect(response).to have_http_status(:success)
     end
+
+    it "returns 200 response on successful calls" do
+    	get :search, :doctor_name => 'ted'
+    	expect(response).to have_http_status(200)
+    end
+
+    it "returns success when no search record found" do
+    	get :search, :doctor_name => 'xyz'
+    	expect(response).to have_http_status(:success)
+    	expect(response.status).to eq 200
+	end
+
+
   end
 
 end
